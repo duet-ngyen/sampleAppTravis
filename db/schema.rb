c99241a9cbd65da32b9acd8d14a900fe40acd469
Duyet Nguyen <duynguyenvan@pentalog.com>
2018-05-04 14:30:26 +0700

    add scaffold category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20180504023700) do
+ActiveRecord::Schema.define(:version => 20180504073013) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(:version => 20180504023700) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
