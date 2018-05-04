48d5190d30de35fc548d046d324ae134f058a7d3
Duyet Nguyen <duynguyenvan@pentalog.com>
2018-05-04 10:01:03 +0700

    .

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Author, :type => :model do
   context "test author" do
     it "get name" do
       book = Book.firstRecord
-      binding.pry
       expect(book).to eq(Book.first)
     end
   end
