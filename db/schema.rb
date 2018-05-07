ecf63bdbf80b7ac53ecfb488a7a2bb57003cd449
Duyet Nguyen <duynguyenvan@pentalog.com>
2018-05-07 11:11:07 +0700

    addslack
    cc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20180504073013) do
+ActiveRecord::Schema.define(:version => 20180507033207) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(:version => 20180504073013) do
   end
 
   create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "teachers", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
